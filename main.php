82e301cd64d81df48ce1023d0d76da1b35025f7f
jamesstandbridge <james.standbridge.git@gmail.com>
2021-04-02 16:01:40 +0200

    info v2

--- main.php
+++ main.php
@@ -6,5 +6,5 @@ require 'vendor/autoload.php';
 use UniversalConnector\API\APIProvider;
 
 $api = APIProvider::Magento2Instance();
-$api->initialize("sgagence", "Boeki2019@", "https://www.centrale-ethnique.com");
+$api->initialize("");
 $api->postTierPrice();
