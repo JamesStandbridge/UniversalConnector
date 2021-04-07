3fba774fd3bd23f077ad1d07b1bd6cdd6729fdd7
jamesstandbridge <james.standbridge.git@gmail.com>
2021-04-07 15:15:23 +0200

    fix: context website id

--- src/API/Magento2/Context.php
+++ src/API/Magento2/Context.php
@@ -7,10 +7,7 @@ class Context {
 
 	private $website_id;
 
-	public function __construct() 
-	{
-		$this->website_id = $website_id;
-	}
+	public function __construct() {}
 
 	public function setWebsiteID(int $website_id) 
 	{
